8139d7142487a8f2f8ee441f5bc8bd53a64c7f17
tomlenton <twlenton@gmail.com>
2026-04-24 14:31:17 +0100

    feat: Added GetAllAuthors

--- cs-MVC-sprint/Services/AuthorService.cs
+++ cs-MVC-sprint/Services/AuthorService.cs
@@ -1,6 +1,24 @@
-﻿namespace cs_MVC_sprint.Services
+﻿using cs_MVC_sprint.Models;
+
+
+
+namespace cs_MVC_sprint.Services
 {
-    public class AuthorService
+    public interface IAuthorService
     {
+        List<Author> GetAllAuthors();
+    }
+    public class AuthorService : IAuthorService
+    {
+
+        public AuthorModel _authorModel;
+        public AuthorService (AuthorModel authorModel)
+        {
+            _authorModel = authorModel;
+        }
+        public List<Author> GetAllAuthors()
+        {
+            return _authorModel.GetAllAuthors();
+        }
     }
 }
