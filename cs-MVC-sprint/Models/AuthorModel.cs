90b9764c9487b9e4823e35f5899b001edad510d1
tomlenton <twlenton@gmail.com>
2026-04-24 14:32:27 +0100

    feat: Added GetAllAuthors

---
+++ cs-MVC-sprint/Models/AuthorModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace cs_MVC_sprint.Models
+{
+    public interface IAuthorModel
+    {
+        List<Author> GetAllAuthors();
+    }
+    public class AuthorModel : IAuthorModel
+    {
+        public List<Author> GetAllAuthors()
+        {
+            var filepath = "Resources/Authors.json";
+            var json = File.ReadAllText(filepath);
+            List<Author>? authors = JsonSerializer.Deserialize<List<Author>>(json);
+
+            return authors.Select(author => new Author
+            {
+                Id = author.Id,
+                Name = author.Name,
+                Nationality = author.Nationality
+            }).ToList();
+
+        }
+    }
+}
